50351d8e94df7b19b5c2bb702e8dc3d867662ba4
xs <laozishixs@gmail.com>
2017-02-21 11:42:58 +0100

    v0.0.1

---
+++ StromZ/StromZ/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace StromZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_viewrecord = new System.Windows.Forms.Button();
+            this.lb_watts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_viewrecord
+            // 
+            this.btn_viewrecord.Location = new System.Drawing.Point(181, 227);
+            this.btn_viewrecord.Name = "btn_viewrecord";
+            this.btn_viewrecord.Size = new System.Drawing.Size(91, 23);
+            this.btn_viewrecord.TabIndex = 0;
+            this.btn_viewrecord.Text = "Rekord Lesen";
+            this.btn_viewrecord.UseVisualStyleBackColor = true;
+            // 
+            // lb_watts
+            // 
+            this.lb_watts.AutoSize = true;
+            this.lb_watts.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_watts.Location = new System.Drawing.Point(71, 97);
+            this.lb_watts.Name = "lb_watts";
+            this.lb_watts.Size = new System.Drawing.Size(146, 46);
+            this.lb_watts.TabIndex = 1;
+            this.lb_watts.Text = "0000W";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lb_watts);
+            this.Controls.Add(this.btn_viewrecord);
+            this.Name = "Form1";
+            this.Text = "Stromzähler für EnBW";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_viewrecord;
+        private System.Windows.Forms.Label lb_watts;
+    }
+}
+
---
+++ StromZ/StromZ/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StromZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread tDeamon = new Thread(new ThreadStart(this.refreshWatt));
+            tDeamon.Start();
+        }
+        private void refreshWatt()
+        {
+            while (true)
+            {
+                System.Net.WebClient client = new WebClient();
+                byte[] page = client.DownloadData("http://192.168.0.100/");
+                string content = System.Text.Encoding.UTF8.GetString(page);
+                Regex re = new Regex(@"<div class=""whats"">(.*)</div>");
+                Match m = re.Match(content);
+                if (m.Success)
+                {
+                    //safe thread update label
+                    if (this.lb_watts.InvokeRequired)
+                    {
+                        Action<string> actionDelegate = delegate (string txt) { this.lb_watts.Text = txt; };
+                        this.lb_watts.Invoke(actionDelegate, m.Groups[1].ToString());
+                    }
+                    else
+                    {
+                        this.lb_watts.Text = m.Groups[1].ToString();
+                    }
+                    //record into a file
+                    Console.Write(m.Groups[1] + "\r\n");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
